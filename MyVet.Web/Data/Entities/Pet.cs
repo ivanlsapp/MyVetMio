8bba8206c86c0545b29ebf29ce4fd1a3c9a1c845
iva29 <iva29@IVAN-PC>
2019-08-22 18:10:42 +0200

    Tuto 58 - 20 -Funcionando

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -10,7 +10,7 @@ namespace MyVet.Web.Data.Entities
 
         [Display(Name = "Name")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Required(ErrorMessage = "The field {0} is necessary.")]
         public string Name { get; set; }
 
         [Display(Name = "Image")]
@@ -21,7 +21,6 @@ namespace MyVet.Web.Data.Entities
 
         [Display(Name = "Born")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public DateTime Born { get; set; }
 
---
+++ MyVet.Web/Helpers/ICombosHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyVet.Web.Data
+{
+    public interface ICombosHelper
+    {
+        IEnumerable<SelectListItem> GetComboPetTypes();
+
+        IEnumerable<SelectListItem> GetComboServiceTypes();
+    }
+}
