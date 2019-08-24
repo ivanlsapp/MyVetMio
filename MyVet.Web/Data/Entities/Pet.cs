bdd90dde38704a31ea4ba8668406c0838f99d42e
iva29 <iva29@IVAN-PC>
2019-08-24 21:38:34 +0200

    Publish en Azure

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -26,10 +26,9 @@ namespace MyVet.Web.Data.Entities
 
         public string Remarks { get; set; }
 
-        //TODO: replace the correct URL for the image
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
             ? null
-            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://myvet1.azurewebsites.net{ImageUrl.Substring(1)}";
 
 
         [Display(Name = "Born")]
